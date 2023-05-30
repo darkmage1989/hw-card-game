a5cc8ab66be9b5a00e8e20d8450ef75cfb74d6c7
darkmage1989 <darkmage1989@gmail.com>
2023-05-30 23:36:05 +0300

    просто для коммита

--- src/js/script.test.js
+++ src/js/script.test.js
@@ -1,6 +1,8 @@
 const { it, describe } = require('@jest/globals');
 const { shuffle } = require('./units');
 
+// моя ненависть к jest бесконечна
+
 describe('Не пропадают ли значения', () => {
     it('god help me', () => {
         const numbers = [1, 2, 3, 4, 5];
